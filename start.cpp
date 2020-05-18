19d873326541694a07715a36485e22ada360b198
tdscheper <59904464+tdscheper@users.noreply.github.com>
2020-05-18 14:58:35 -0400

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -16,25 +16,6 @@ void startTests();
 int main() {
     cout << "Welcome to Acey Deucey!\nmade by Tommy Scheper\n";
     
-    /*
-    cout << "\nWhat do you want to do?\n";
-    cout << "1) Play Acey Deucey\n2) Run tests\n";
-    cout << "\nChoice --> ";
- 
-    int choice;
-    cin >> choice;
-    string junk;
-    getline(cin, junk);
- 
-    if (choice == 1) {
-        Game g1;
-        g1.play();
-    }
-    else if (choice == 2) {
-        startTests();
-    }
-    */
-    
     Game g;
     g.play();
     
